d87a60e6c81bd7af0ccfa0afbc33e0d930c78b16
Lucas Panzera <lucassouzapanzera@gmail.com>
2024-11-29 13:08:38 -0300

    Mensagens no sistema

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,9 +1,21 @@
+import { useLocation } from 'react-router-dom'
+
 import styles from './Projects.module.css'
 
+import Message from '../layouts/Message'
+
 function Projects() {
+
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message = location.state.message
+    }
+
     return (
         <div className={styles.Projects}>
-            <h1>Projects</h1>
+            <h1>My Projects</h1>
+            {message && <Message type="success" msg={message} />}
         </div>
     )
 }
